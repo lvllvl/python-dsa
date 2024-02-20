deef2fd7ac2151943a4419b29a575bc5bad20f90
lvllvl <chrisidxloop@gmail.com>
2024-02-19 20:00:30 -0500

    next commit

---
+++ src/data_structures/LinkedList.py
@@ -0,0 +1,10 @@
+class node:
+    def __init__(self, data = None ):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    
+    def __init__( self ):
+        pass 
+    # end __init__
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from data_structures.LinkedList import LinkedList
+
+
+def main():
+    print("This is the main function.")
+
+if __name__ == "__main__":
+    print("Hello, World!")
+    main()
